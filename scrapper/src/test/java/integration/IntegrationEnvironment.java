0a8bd0d880db1ffc73e544866670884dfba1875c
Chapalax <max250903@gmail.com>
2023-05-02 23:27:35 +0300

    Fixed tests.

--- scrapper/src/test/java/integration/IntegrationEnvironment.java
+++ scrapper/src/test/java/integration/IntegrationEnvironment.java
@@ -31,7 +31,6 @@ public abstract class IntegrationEnvironment {
             .getParent()
             .resolve("migrations");
 
-
     static {
         SQL_CONTAINER = new PostgreSQLContainer<>("postgres:15");
         SQL_CONTAINER.start();
