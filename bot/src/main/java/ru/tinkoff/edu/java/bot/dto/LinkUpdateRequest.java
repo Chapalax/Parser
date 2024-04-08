b2665a5e4c3a216e505cc5e11417532681953533
chrxn1c <rivalofficialentertainment@gmail.com>
2024-04-08 20:47:40 +0300

    author and version of LinkUpdateRequest added

--- bot/src/main/java/ru/tinkoff/edu/java/bot/dto/LinkUpdateRequest.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/dto/LinkUpdateRequest.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 
 /**
  * DTO designed for updating requests of users for given tgChatsIds with given description, url, and (what id?)
+ * @author Maxim Berezhnoy
+ * @version 1.0
 */
 public record LinkUpdateRequest(long id, @URL String url, String description, ArrayList<Long> tgChatIds) {
 }
