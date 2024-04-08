8cf8610267017cab4b5870cd2fb64b1b2e7171f8
chrxn1c <rivalofficialentertainment@gmail.com>
2024-04-08 20:46:53 +0300

    docs: description of dtos added

--- bot/src/main/java/ru/tinkoff/edu/java/bot/dto/LinkUpdateRequest.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/dto/LinkUpdateRequest.java
@@ -4,5 +4,8 @@ import org.hibernate.validator.constraints.URL;
 
 import java.util.ArrayList;
 
+/**
+ * DTO designed for updating requests of users for given tgChatsIds with given description, url, and (what id?)
+*/
 public record LinkUpdateRequest(long id, @URL String url, String description, ArrayList<Long> tgChatIds) {
 }
