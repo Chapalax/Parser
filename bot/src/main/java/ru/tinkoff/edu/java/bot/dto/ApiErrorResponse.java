7915c0599b48df801adc55992fa10659ae3e3a37
chrxn1c <rivalofficialentertainment@gmail.com>
2024-04-09 21:00:41 +0300

    docs: documentation of bot dtos is fixed

--- bot/src/main/java/ru/tinkoff/edu/java/bot/dto/ApiErrorResponse.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/dto/ApiErrorResponse.java
@@ -6,6 +6,11 @@ import java.util.ArrayList;
  * DTO representing an error generated in the process of processing user request.
  * @author Maxim Berezhnoy
  * @version 1.0
+ * @param description description of what had happened wrong
+ * @param code status code returned
+ * @param exceptionName name of java's exception that has been generated
+ * @param exceptionMessage description of generated exception
+ * @param stacktrace stacktrace of the exception
  */
 public record ApiErrorResponse(String description, String code, String exceptionName,
                                String exceptionMessage, ArrayList<String> stacktrace) {
--- bot/src/main/java/ru/tinkoff/edu/java/bot/dto/LinkUpdateRequest.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/dto/LinkUpdateRequest.java
@@ -5,9 +5,13 @@ import org.hibernate.validator.constraints.URL;
 import java.util.ArrayList;
 
 /**
- * DTO designed for updating requests of users for given tgChatsIds with given description, url, and (what id?)
+ * DTO designed for updating requests of users for given tgChatsIds with given description, url, and id of database
  * @author Maxim Berezhnoy
  * @version 1.0
-*/
+ * @param id id of database
+ * @param url url of repository
+ * @param description text of what has been updated
+ * @param tgChatIds what users have to be notified of incoming updates in repository
+ */
 public record LinkUpdateRequest(long id, @URL String url, String description, ArrayList<Long> tgChatIds) {
 }
