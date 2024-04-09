82648be003b032a9e15e5bc38af56151b98b4c86
chrxn1c <rivalofficialentertainment@gmail.com>
2024-04-09 22:40:33 +0300

    docs: documentation of bot telegram interfaces added

--- bot/src/main/java/ru/tinkoff/edu/java/bot/telegram/interfaces/Bot.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/telegram/interfaces/Bot.java
@@ -5,12 +5,26 @@ import com.pengrad.telegrambot.model.Update;
 
 import java.util.List;
 
+/**
+ * Interface for bot defining common actions of what bot can be doing
+ * @author Maxim Berezhnoy
+ * @version 1.0
+ */
 public interface Bot extends AutoCloseable, UpdatesListener {
+    /**
+     * Method for starting the bot
+     */
     void start();
 
+    /**
+     * Method for stopping receiving updates from users
+     */
     @Override
     void close() throws Exception;
 
+    /**
+     * Method for processing the updates sent from Telegram API
+     */
     @Override
     int process(List<Update> list);
 }
--- bot/src/main/java/ru/tinkoff/edu/java/bot/telegram/interfaces/UserMessageProcessor.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/telegram/interfaces/UserMessageProcessor.java
@@ -5,10 +5,28 @@ import com.pengrad.telegrambot.request.SendMessage;
 
 import java.util.List;
 
+/**
+ * Interface representing how user messages can be processed via bot
+ * @author Maxim Berezhnoy
+ * @version 1.0
+ */
 public interface UserMessageProcessor {
+    /**
+     * Commands that can be applied to bot by user
+     * @return List of commands
+     */
     List<? extends Command> commands();
 
+    /**
+     * Method for processing user input to bot
+     * @param update Message from user to bot
+     * @return Message that is sent back to user as result of processing user's command
+     */
     SendMessage process(Update update);
 
+    /**
+     * Dispose user from users tracked by bot
+     * @param update Message from user to bot
+     */
     void deleteChat(Update update);
 }
