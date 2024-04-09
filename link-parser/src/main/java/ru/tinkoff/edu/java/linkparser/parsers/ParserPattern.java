93be9fd739237364b448b115d1688ae2cdccbed6
chrxn1c <rivalofficialentertainment@gmail.com>
2024-04-10 00:20:02 +0300

    docs: linkparser documentation is added, tests were left untouched.

--- link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parsers/ParserPattern.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parsers/ParserPattern.java
@@ -2,6 +2,23 @@ package ru.tinkoff.edu.java.linkparser.parsers;
 
 import java.net.URI;
 
+import ru.tinkoff.edu.java.linkparser.records.ParsedGitHub;
+import ru.tinkoff.edu.java.linkparser.records.ParsedStackOverflow;
+
+/**
+ * Common interface for parser to parse sites GitHub and StackOverflow.
+ * Only {@link GitHubParser} and {@link StackOverflowParser} could be implementing this interface
+ * 
+ * @see GitHubParser
+ * @see StackOverflowParser
+ * @author Maxim Berezhnoy
+ * @version 1.0
+ */
 public sealed interface ParserPattern permits GitHubParser, StackOverflowParser {
+    /**
+     * Method responsible for parsing defined URL
+     * @param url URL of page to be parsed
+     * @return Record of parsed page. Its type is either {@link ParsedGitHub} or {@link ParsedStackOverflow}
+     */
     Record parse(URI url);
 }
--- link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/records/ParsedGitHub.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/records/ParsedGitHub.java
@@ -1,4 +1,12 @@
 package ru.tinkoff.edu.java.linkparser.records;
 
+/**
+ * DTO representing parsed GitHub page of repository
+ * @param user User to which repository belongs
+ * @param repository Repository name
+ * 
+ * @author Maxim Berezhnoy
+ * @version 1.0
+ */
 public record ParsedGitHub(String user, String repository) {
 }
