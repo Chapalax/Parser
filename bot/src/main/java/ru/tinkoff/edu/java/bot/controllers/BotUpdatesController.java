2a993de9ad365a7ea3cf55506afbaf20c7776cea
chrxn1c <rivalofficialentertainment@gmail.com>
2024-04-09 20:59:19 +0300

    docs: documentation of bot controllers is added

--- bot/src/main/java/ru/tinkoff/edu/java/bot/controllers/BotUpdatesController.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/controllers/BotUpdatesController.java
@@ -10,10 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.tinkoff.edu.java.bot.dto.LinkUpdateRequest;
 import ru.tinkoff.edu.java.bot.telegram.TrackerBot;
 
+
+/**
+ * Controller for handling /updates endpoint
+ * @author Maxim Berezhnoy
+ * @version 1.0
+ */
 @RestController
 @RequestMapping("/updates")
 public class BotUpdatesController {
 
+    /**
+     * @param linkUpdate - link to which bot sends updates to user of user's chosen repo 
+     * @return - HTTP status 200
+     */
     @PostMapping
     public ResponseEntity<HttpStatus> postSendUpdate(@RequestBody @Valid LinkUpdateRequest linkUpdate) {
         TrackerBot.sendUpdates(linkUpdate);
