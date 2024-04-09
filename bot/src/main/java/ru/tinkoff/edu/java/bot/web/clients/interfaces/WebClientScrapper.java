da402d1a480f259502ce658d31a7fa5df84b2d50
chrxn1c <rivalofficialentertainment@gmail.com>
2024-04-09 21:11:31 +0300

    docs: documentation for web clients interfaces is added

--- bot/src/main/java/ru/tinkoff/edu/java/bot/web/clients/interfaces/WebClientScrapper.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/web/clients/interfaces/WebClientScrapper.java
@@ -6,9 +6,27 @@ import ru.tinkoff.edu.java.bot.web.clients.dto.LinkResponse;
 import ru.tinkoff.edu.java.bot.web.clients.dto.ListLinksResponse;
 import ru.tinkoff.edu.java.bot.web.clients.dto.RemoveLinkRequest;
 
+/**
+ * Base interface for defining how scrapper can interact with Telegram API
+ * @author Maxim Berezhnoy
+ * @version 1.0
+ */
 public interface WebClientScrapper {
+    /**
+     * Get list of all repositories attached to a client
+     * @param tgChatId Telegram ID of concrete user
+     * @see ListLinksResponse
+     * @return Entity representing response for getting array of links of repositoriest and its size
+     */
     ListLinksResponse getAllLinks(long tgChatId);
 
+    /**
+     * Add new trackable repository to attached client
+     * @param tgChatId Telegram ID of concrete user
+     * @param addLinkRequest Request for adding a new repository to be tracked
+     * @see AddLinkRequest
+     * @return Entity representing response for interacting with 
+     */
     LinkResponse addLink(long tgChatId, AddLinkRequest addLinkRequest);
 
     LinkResponse deleteLink(long tgChatId, RemoveLinkRequest removeLinkRequest);
